0b8f44f2d854996d0746404fc0302014e7dc3eeb
sehesofe <sehesofe@gmail.com>
2022-10-13 10:38:08 -0300

    Tables and example were included

---
+++ example/run_example.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+export MBUFR_TABLES=../bufrtables
+#--------------------------------------------
+# Encode daycli message im BUFR from text file
+#----------------------------------------------
+../bin/daycli_encoder -i DAYCLI_82191.txt -o DAYCLI_82191.bufr -s 001 -h "ISXX01 SBBR 202000"
+
+#-------------------------------------------------------------
+# Decode DAYCLI message from BUFR to text file (using bufrdump)
+#--------------------------------------------------------------
+../bin/bufrdump -i DAYCLI_82191.bufr -o DAYCLI_82191.bufr.txt
+
+#-------------------------------------------------------------
+# Decode DAYCLI message from BUFR to csv file (using bufr2csv)
+#--------------------------------------------------------------
+../bin/bufr2csv DAYCLI_82191.bufr 0 0
